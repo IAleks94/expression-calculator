7338b26a0bdd90518c746e0ffd50f6a58050870c
IAleks94 <a12344@mail.ru>
2020-03-05 09:39:54 +0300

    delite tresh

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
-function eval() {
-  // Do not use eval!!!
-  return;
-}
-
 function expressionCalculator(expr) {
- // let regExpArrFirst = [/\([^\(]*\)/,
+
  let regExpArrFirst = [/\([^\(\)]*\)/,
   /(?<leftNum>\-?\d+(\.\d+)?\b)[\s+]?(?<sing>[/*])[\s+]?(?<rightNum>\-?\d+(\.\d+)?\b)/, 
   /(?<leftNum>\-?\d+(\.\d+)?\b)[\s+]?(?<sing>[+-])[\s+]?(?<rightNum>\-?\d+(\.\d+)?\b)/];
   for (let i = 0; i < 3; i++) {
-   // console.log('regExpArrFirst ' +regExpArrFirst[i]);
-    //console.log('expr ' + expr);
+
     let miniExpr = expr.match(regExpArrFirst[i]);
-  //console.log('miniExpr ' +miniExpr);
+
     if (i === 0 && miniExpr) {
-      //let str = miniExpr[0].match(/[^\(].+[^\)]/)+'';
       let str = miniExpr[0].slice(1, -1);
-    //  console.log('str ' + str);
     expr = expr.replace(regExpArrFirst[i],  expressionCalculator(str))+'';
-   // console.log('получаем ' + expr);
     return expressionCalculator(expr);
     } else if (miniExpr == null) {
       continue;
     }
-  //  console.log(regExpArrFirst[i]);
-    //console.log("miniExpr: ", miniExpr);
     if (miniExpr) {
-    //  console.log(miniExpr.groups);
-     // console.log(miniExpr);
       let {leftNum , rightNum, sing} = miniExpr.groups;
         if (sing) {
           let newExpr = expr.replace(
             regExpArrFirst[i],
             caclulator(leftNum, sing[0], rightNum)
           );
-         //console.log('newExpr= ' +newExpr);
           return expressionCalculator(newExpr);
         }   
     }
@@ -42,7 +28,6 @@ function expressionCalculator(expr) {
   if (Number(expr) || expr == 0) {
     return Number(expr);
   } else if (expr.match(/\s/)) {
-    //  console.log(expr);
       expr = expr.split(' ').join('');
     return expressionCalculator(expr);
     } else if (isNaN(expr)){
@@ -52,25 +37,19 @@ function expressionCalculator(expr) {
   
 
   function caclulator(left, sing, right) { // возвращяем строку
-   // console.log(`${left} ${sing} ${right}`);
     switch (sing) {
       case "+":
         return (+left + (+right))+'';
-        break;
       case "-":
         return (+left - +right)+'';
-        break;
       case "*":
         return (+left * +right)+'';
-        break;
       case "/":
         if (!+right) {
           throw new Error("TypeError: Division by zero.");
         } else {
           return (+left / +right).toFixed(15)+'';
         }
-
-        break;
     }
   }
 }
@@ -79,19 +58,4 @@ module.exports = {
   expressionCalculator
 };
 
- //  const expr = " (  38 + 52 + 65 - 19  ) * (  72 * 3 / 36 * (  9 / 2 - 17 * 38 / 28  )  ) / 18 / 84 ";
-// // // const result = -10.0227;
 
-  //  let answer = expressionCalculator(expr);
- //   console.log('answer ' +answer);
-// //   console.log( typeof answer);
-
-
-// console.log('isNaN("2 +2"): ', isNaN(' 2 +2'));
-
-//  function sum(num) {
-//    num *2
-//    return sum(num*2);
-//  }
-
-//console.log(Number(+'-51' + +'0.8181818181'));
